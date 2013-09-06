eda1c619eb19f19d5faa7d8049880cafdbfd7c49
Ismael Figueroa <ifigueroap@gmail.com>
2013-09-06 19:36:35 -0400

    quickcheck again

---
+++ play/quickcheck.rkt
@@ -0,0 +1,17 @@
+#lang setup/infotab
+
+(define blurb '("PLAY QuickCheck"))
+(define homepage "http://dcc.uchile.cl/~etanter/")
+(define primary-file "quickcheck.rkt")
+
+;;(define scribblings '(("scribblings/play.scrbl" (multi-page) (teaching -20))))
+
+(define compile-omit-files
+  '("examples.scm"
+    "packages.scm"
+    "quickcheck-test.scm"
+    "quickcheck.scm"
+    "random.scm"))
+
+
+
