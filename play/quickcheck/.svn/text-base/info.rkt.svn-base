5e27138da5e76121fe5119f03f167a2df09d0313
Ismael Figueroa <ifigueroap@gmail.com>
2013-09-06 19:26:31 -0400

    moved quickcheck to sub-collect

---
+++ play/quickcheck/.svn/text-base/info.rkt.svn-base
@@ -0,0 +1,12 @@
+(module info setup/infotab
+  (define name "DeinProgramm - QuickCheck")
+
+  (define compile-omit-files
+    '("examples.scm"
+      "packages.scm"
+      "quickcheck-test.scm"
+      "quickcheck.scm"
+      "random.scm")))
+
+
+  
---
+++ play/quickcheck/info.rkt
@@ -0,0 +1,15 @@
+#lang setup/infotab
+
+(define blurb '("DeinProgramm - QuickCheck"))
+;(define homepage "http://dcc.uchile.cl/~etanter/")
+(define primary-file "quickcheck.rkt")
+
+(define compile-omit-files
+  '("examples.scm"
+    "packages.scm"
+    "quickcheck-test.scm"
+    "quickcheck.scm"
+    "random.scm"))
+
+
+
