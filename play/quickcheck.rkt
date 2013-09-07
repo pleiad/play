96d6504b85a7e7ec9d03d8536165907d01e0e689
Ismael Figueroa <ifigueroap@gmail.com>
2013-09-06 20:05:16 -0400

    fixing provides

--- play/quickcheck.rkt
+++ play/quickcheck.rkt
@@ -1,17 +1,4 @@
-#lang setup/infotab
+#lang racket
 
-(define blurb '("PLAY QuickCheck"))
-(define homepage "http://dcc.uchile.cl/~etanter/")
-(define primary-file "quickcheck/quickcheck.rkt")
-
-;;(define scribblings '(("scribblings/play.scrbl" (multi-page) (teaching -20))))
-
-(define compile-omit-files
-  '("examples.scm"
-    "packages.scm"
-    "quickcheck-test.scm"
-    "quickcheck.scm"
-    "random.scm"))
-
-
-
+(require "quickcheck/quickcheck.rkt")
+(provide (all-from-out "quickcheck/quickcheck.rkt"))
