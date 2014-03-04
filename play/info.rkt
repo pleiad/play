96e6c86b959fe7c4445e2d8f20023852d993345e
Ismael Figueroa <ifigueroap@gmail.com>
2014-03-04 16:10:15 -0300

    Restructuring

--- play/info.rkt
+++
@@ -1,7 +0,0 @@
-#lang setup/infotab
-
-(define blurb '("PLAY"))
-(define homepage "http://dcc.uchile.cl/~etanter/")
-(define primary-file "main.rkt")
-
-;;(define scribblings '(("scribblings/play.scrbl" (multi-page) (teaching -20))))
--- play/quickcheck.rkt
+++
@@ -1,4 +0,0 @@
-#lang racket
-
-(require "quickcheck/quickcheck.rkt")
-(provide (all-from-out "quickcheck/quickcheck.rkt"))
