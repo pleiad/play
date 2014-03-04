3fc3f77fd91319bc35521d51564d4cc35ba7459e
Ismael Figueroa <ifigueroap@gmail.com>
2014-03-04 17:03:05 -0300

    Deps

--- info.rkt
+++ info.rkt
@@ -4,4 +4,6 @@
 (define homepage "http://dcc.uchile.cl/~etanter/")
 (define primary-file "main.rkt")
 
+(define deps '("base" "plai"))
+
 ;;(define scribblings '(("scribblings/play.scrbl" (multi-page) (teaching -20))))
