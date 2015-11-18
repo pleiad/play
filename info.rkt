16356e9a4268216fe1dc2eaa018d4d441e29c91f
Racket Package Dependency Fixer <jackhfirth+racket-dep-fixer@gmail.com>
2015-11-18 02:01:33 +0000

    Fix dependencies.

--- info.rkt
+++ info.rkt
@@ -4,6 +4,7 @@
 (define homepage "http://dcc.uchile.cl/~etanter/")
 (define primary-file "main.rkt")
 
-(define deps '("base" "plai" "redex"))
+(define deps '("rackunit-lib"
+               "base" "plai" "redex"))
 
 ;;(define scribblings '(("scribblings/play.scrbl" (multi-page) (teaching -20))))
